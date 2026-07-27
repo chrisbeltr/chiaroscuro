869a5186c9ab0d991d1874ca9cac491551418a50
chrisbeltr <christianbeltran@uchicago.edu>
2026-07-27 00:55:16 -0500

    removed console writes

--- src/Chiaroscuro.UI/ViewModels/MainViewModel.cs
+++ src/Chiaroscuro.UI/ViewModels/MainViewModel.cs
@@ -158,8 +158,8 @@ public partial class MainViewModel : ViewModelBase
     [RelayCommand]
     private void JumpToNow()
     {
-        Console.WriteLine("waf waf");
         SelectedAlignmentMatch = null;
+
         var now = DateTimeOffset.Now;
         Date = now.Date;
         TimeOfDay = now.TimeOfDay;
@@ -190,8 +190,6 @@ public partial class MainViewModel : ViewModelBase
             return;
         }
         
-        Console.WriteLine("meow");
-
         Date = new DateTimeOffset(d.Date, TimeSpan.Zero);
         TimeOfDay = d.TimeOfDay;
     }
@@ -270,7 +268,6 @@ public partial class MainViewModel : ViewModelBase
 
             if (!tempMatches.SequenceEqual(AlignmentMatches))
             {
-                Console.WriteLine("WHYYYY");
                 AlignmentMatches = tempMatches;
             }
         }
