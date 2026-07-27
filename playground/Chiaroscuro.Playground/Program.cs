ad220d38f9864668ee4c4fed77552ea1dd39f08f
chrisbeltr <christianbeltran@uchicago.edu>
2026-07-27 16:50:55 -0500

    removed unnecessary development and planning files

--- playground/Chiaroscuro.Playground/Program.cs
+++
@@ -1,8 +0,0 @@
-using Chiaroscuro.Core.Solar;
-using NodaTime;
-
-var dateTime = SystemClock.Instance.GetCurrentInstant().InUtc();
-var position = SolarCalculator.Calculate(latitudeDegrees: 40.7128, longitudeDegrees: -74.0060, dateTime);
-
-Console.WriteLine($"Elevation: {position.ElevationDegrees:F2}°, Azimuth: {position.AzimuthDegrees:F2}°");
-Console.WriteLine($"Sun unit vector: {position.ToUnitVector()}");
